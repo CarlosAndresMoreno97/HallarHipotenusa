1d35a257a9e92414140dc7ab874518e028f8431b
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2023-09-01 16:26:33 -0500

    Hallar hipotenusa

---
+++ HallarHipotenusa/Program.cs
@@ -0,0 +1,24 @@
+﻿//Enunciado: 5.Dados los catetos de un triángulo rectángulo, calcular su hipotenusa.
+
+public class hallarhipotenusa
+{
+
+
+    static void Main(string[] args)
+    {
+        double catetoO, catetoA, Hipotenusa;
+
+        Console.Write("Ingresar cateto opuesto: ");
+        catetoO = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresar cateto adyacente: ");
+        catetoA = Convert.ToDouble(Console.ReadLine());
+
+        Hipotenusa = Math.Sqrt(Math.Pow(catetoO, 2) + Math.Pow(catetoA, 2));
+
+        Console.Write("La hipotenusa es igual a: " + Hipotenusa);
+        Console.Write("\n");
+
+    }
+
+}
